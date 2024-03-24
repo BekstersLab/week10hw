80193fdfaaa1fc72a979e00c69e51107707b1af7
Bek <rebecca.e.rootes@gmail.com>
2024-03-24 11:44:32 +0000

    added comments throughout

--- FlaskWeek10_Bek/app.py
+++ FlaskWeek10_Bek/app.py
@@ -1,18 +1,32 @@
+# import flask class and url_for function from flask library
 from flask import Flask, url_for
 
-# instantiate a Flask object
+# instantiate a Flask object (instance of Flask class)
+# '__name__' is special variable giving Python files a unique name to correctly locate them
 app = Flask(__name__)
 
 
-# a get route by default
-@app.route('/')
-@app.route('/home')
-def home_page():
-    home_url = url_for('home_page')
-    about_url = url_for('about_page')
-    portfolio_url = url_for('portfolio_page')
-    contact_url = url_for('contact_page')
-    return f"""
+# a get route by default, unless specified otherwise
+@app.route('/')  # decorator that tells Flask which URL should trigger the function below
+@app.route('/home')  # can have more than one decorator allowing function to be accessible from different url
+def home_page():  # define function called when root or /home url is accessed
+    # 'url_for()' dynamically builds urls for specific functions
+    # takes name of function you want to generate an url for as an argument
+    # avoids hard coding urls so if route is changed the urls don't need manually updating throughout app
+    # 'url_for()' automatically generates correct url based on new route
+    home_url = url_for('home_page')  # 'url_for()' dynamically generates url for home_page and stores in a variable
+    about_url = url_for('about_page')  # generates url for about page
+    portfolio_url = url_for('portfolio_page')  # generates url for portfolio page
+    contact_url = url_for('contact_page')  # generates url for contact page
+
+    # within this function, 'home_url = url_for('home_page')' generates an url path for the home_page function and ...
+    # assigns it to the home_url variable. The url path corresponds to '@app.route('/')' or '@app.route(/home)' decorator
+    # the variables are used to create hyperlinks (<a></a>) within the navigation section of each page
+    # e.g. '{home_url}' is replaced with the generated url for the home page when Python code is executed
+
+    # HTML string below is returned and rendered in the browser
+    # f-string with triples quotes as it is multiline
+    return f""" 
     <!doctype>
     <html>
         <head>
@@ -38,12 +52,15 @@ def home_page():
     """
 
 
+# structure is similar for about_page
+# the decorator tells Flask the about url should trigger the function below
 @app.route('/about')
-def about_page():
-    home_url = url_for('home_page')
+def about_page():  # define the function for the about page
+    home_url = url_for('home_page')  # identical to code in home_page function
     about_url = url_for('about_page')
     portfolio_url = url_for('portfolio_page')
     contact_url = url_for('contact_page')
+    # HTML structure is the same with changes only to title and written content relevant to the page
     return f"""
     <!doctype>
     <html>
@@ -72,6 +89,7 @@ def about_page():
     """
 
 
+# repeat the structure for portfolio_page
 @app.route('/portfolio')
 def portfolio_page():
     home_url = url_for('home_page')
@@ -104,6 +122,7 @@ def portfolio_page():
     """
 
 
+# repeat the structure for contact_page
 @app.route('/contact')
 def contact_page():
     home_url = url_for('home_page')
@@ -135,6 +154,19 @@ def contact_page():
     </html>     
     """
 
+
+# '__name__' is special built-in variable and when script is executed Python sets '__name__' variable to '"__main__"' if script is being run as main program
+# if script is imported as a module into another script then '__name__' is set to the script/module name
+# first line checks if script is being run directly by the Python interpreter and condition is true if the script is executed as the main program/entry point of app
+if __name__ == "__main__":
+    # app object is instance of the Flask class, run tells Flask to start serving application to handle incoming web requests
+    # 'debug=True' argument enables debug mode within the Flask application
+    # - automatically reloads Flask application if you make changes to the code
+    # - interactive debugger shown in the web browser
+    app.run(debug=True)
+
+
+# # ////// LESSON CODE //////
 # @app.route('/page1/<name>')
 # def simple_html_page(name):
 #     # url_for() is a flask method to get url python function
@@ -176,6 +208,3 @@ def contact_page():
 #     squared = number ** 2
 #     message = "Your number squared is " + str(squared)
 #     return message
-
-if __name__ == "__main__":
-    app.run(debug=True)
